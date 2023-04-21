a5ab7e832e8424301a67a62288baa8ae07027fc2
Anton Dudakov <anton.dudakov@ziina.com>
2023-04-21 14:14:09 +0400

    some clean up

--- src/main/kotlin/1_Scope/Puzzler1_2.kt
+++ src/main/kotlin/1_Scope/Puzzler1_2.kt
@@ -1,19 +1,23 @@
 package `1_Scope1_2`
 
 import kotlinx.coroutines.*
+import utils.threadName
 
 
 // Puzzler 1.2: Coroutine Context Switching
 // Question: What is the output of this code snippet, and why?
+@OptIn(DelicateCoroutinesApi::class)
 fun main(): Unit = runBlocking {
     val context1 = newSingleThreadContext("Context1")
     val context2 = newSingleThreadContext("Context2")
 
     launch(context1) {
-        println("Starting in context1: ${Thread.currentThread().name}")
+        println("Starting in context1: $threadName")
+
         withContext(context2) {
-            println("Switched to context2: ${Thread.currentThread().name}")
+            println("Switched to context2: $threadName")
         }
-        println("Back to context1: ${Thread.currentThread().name}")
+
+        println("Back to context1: $threadName")
     }
 }
