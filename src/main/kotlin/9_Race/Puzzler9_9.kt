ba524661960c6ca98f4e8612e12a533b1d36b1f9
Anton Dudakov <anton.dudakov@ziina.com>
2023-04-21 12:44:44 +0400

    clean up name to have fewer spoilers

--- src/main/kotlin/9_Race/Puzzler9_9.kt
+++ src/main/kotlin/9_Race/Puzzler9_9.kt
@@ -1,10 +1,12 @@
-package `9_Race9_2c`
+package `9_Race9_5`
 
 import kotlinx.coroutines.*
+import kotlinx.coroutines.sync.Mutex
 
 data class Counter(var count: Int = 0)
 
 val counter = Counter()
+val mutex = Mutex()
 
 fun increment() {
     counter.count++
@@ -13,21 +15,29 @@ fun increment() {
 fun main() = runBlocking {
     val jobs = mutableListOf<Job>()
     val customDispatcher = newFixedThreadPoolContext(
-        nThreads = 2,
+        nThreads = 100,
         name = "CustomDispatcher"
     )
 
-    repeat(1_000) {
-        jobs += launch(customDispatcher) {
-            repeat(1_000) {
-                synchronized(counter) {
-                    increment()
-                }
-            }
+    val counterJob = launch {
+        while (isActive) {
+            delay(1)
+            print("Counter: ${counter.count}\r")
         }
     }
 
+    repeat(1_000) {
+        mutex.lock()
+        jobs += launch(customDispatcher) {
+            repeat(1_000) {
+                increment()
+            }
+        }
+        mutex.unlock()
+    }
+
     joinAll(*jobs.toTypedArray())
+    counterJob.cancel()
 
     println("Final count: ${
         counter.count.toString()
