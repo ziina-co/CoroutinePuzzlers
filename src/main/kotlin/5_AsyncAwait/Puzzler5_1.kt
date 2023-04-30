94f12c7a91a6cfce6f1408e86e535795e0f21829
Anton Dudakov <anton.dudakov@ziina.com>
2023-04-30 19:38:46 +0400

    pusslers 5 diagram

--- src/main/kotlin/5_AsyncAwait/Puzzler5_1.kt
+++ src/main/kotlin/5_AsyncAwait/Puzzler5_1.kt
@@ -19,12 +19,36 @@ fun main() = runBlocking {
     val deferred1 = async { performTask(1) }
     val deferred2 = async { performTask(2) }
 
-    print("Waiting for results")
+    print("A ")
     val result1 = deferred1.await()
     val result2 = deferred2.await()
 
-    print(
-        "Results: $result1, $result2.\n" +
-                "⌛️: ${(time.passed)}"
-    )
+    print("$result1 $result2 ${(time.passed)}")
 }
+
+/*
+options:
+ a) A 2 4 1s
+ b) A 1 2 2s
+ c) A 1 2 1s
+ d) A 2 4 2s
+ */
+
+/*
+gitGraph
+    commit id: "start"
+    commit id: "async deferred1"
+    branch Deferred1
+    checkout main
+    commit id: "async deferred2"
+    branch Deferred2
+    checkout Deferred1
+    commit id: "performTask(1)"
+    checkout Deferred2
+    commit id: "performTask(2)"
+    checkout main
+    merge Deferred1 id: "deferred1.await()"
+    commit id: "deferred2.await()"
+    commit id: "print results and time" tag: "Results & Time" type:HIGHLIGHT
+    commit id: "end"
+ */
