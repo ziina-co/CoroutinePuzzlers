86517bb6bb6001d24316b6cc77591c809f03def9
Anton Dudakov <anton.dudakov@ziina.com>
2023-05-08 09:30:34 +0400

    make extensions visible on one line

--- src/main/kotlin/utils/Globals.kt
+++ src/main/kotlin/utils/Globals.kt
@@ -3,6 +3,11 @@ package utils
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
 
-val threadName: String? get() = Thread.currentThread().name
+val threadName: String?
+    get() = Thread
+        .currentThread()
+        .name
 
-fun now(): Duration = System.currentTimeMillis().milliseconds
+fun now(): Duration = System
+    .currentTimeMillis()
+    .milliseconds
