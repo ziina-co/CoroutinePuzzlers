33f1ed5d5fd2eccad97c15f15d2dfca4a6dba389
Anton Dudakov <anton.dudakov@ziina.com>
2024-01-20 08:50:56 +0300

    added some comments

--- src/main/kotlin/utils/models/Counter.kt
+++ src/main/kotlin/utils/models/Counter.kt
@@ -1,6 +1,10 @@
 package utils.models
 
-data class Counter(var count: Int = 0) {
+data class Counter(
+    var count: Int = 0
+) {
+
+    // format number to pretty string
     val pretty: String
         get() = count.toString()
             .reversed()
